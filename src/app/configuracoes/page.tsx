b250bf243320b1f7ac19e55a35935f04a5c674eb
eli-bard <ytbsongbard@gmail.com>
2025-07-05 12:46:08 -0300

    2

--- src/app/configuracoes/page.tsx
+++ src/app/configuracoes/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">Estamos trabalhando</h1>
+        <h1 className="text-4xl font-bold mb-4">Estamos trabalhando parte 1</h1>
         <p className="text-xl">{minhaMensagem}</p>
       </div>
     </main>
